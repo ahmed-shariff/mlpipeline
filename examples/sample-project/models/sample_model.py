49d72bf54af2f15d5466d22709c9d6d66a77edd2
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2018-11-16 17:06:35 +0530

    added more stuff the the example, needs a rivision

--- examples/sample-project/models/sample_model.py
+++ examples/sample-project/models/sample_model.py
@@ -10,64 +10,80 @@ print(sys.path)
 from mlpipeline.utils import add_script_dir_to_PATH
 from mlpipeline.utils import ExecutionModeKeys
 from mlpipeline.utils import Versions
+from mlpipeline.utils import MetricContainer
 from mlpipeline.helper import Model
 from mlpipeline.helper import DataLoader
 
-class An_ML_Model():
-  def __init__(self, hyperparameter="default value"):
-    self.hyperparameter = hyperparameter
 
-  def train(self):
-    return "Trained using {}".format(self.hyperparameter)
+class An_ML_Model():
+    def __init__(self, hyperparameter="default value"):
+        self.hyperparameter = hyperparameter
+
+    def train(self):
+        return "Trained using {}".format(self.hyperparameter)
 
 class TestingDataLoader(DataLoader):
-  def __init__(self):
-    self.log("creating dataloader")
+    def __init__(self):
+        self.log("creating dataloader")
 
-  def get_train_sample_count(self):
-    return 1000
+    def get_train_sample_count(self):
+        return 1000
 
-  def get_test_sample_count(self):
-    return 1000
+    def get_test_sample_count(self):
+        return 1000
 
-  def get_train_input(self, **kargs):
-    return lambda:"got input form train input function"
+    def get_train_input(self, **kargs):
+        return lambda:"got input form train input function"
 
-  def get_test_input(self):
-    return lambda:"got input form test input function"
+    def get_test_input(self):
+        return lambda:"got input form test input function"
   
   
 class TestingModel(Model):
-  def __init__(self, versions, **args):
-    super().__init__(versions, **args)
-    self.model = An_ML_Model()
+    def __init__(self, versions, **args):
+        super().__init__(versions, **args)
+        self.model = An_ML_Model()
     
 
-  def pre_execution_hook(self, version, model_dir, exec_mode=ExecutionModeKeys.TEST):
-    self.log("Pre execution")
-    self.log("Version spec: {}".format(version))
-    self.model.hyperparameter = version["hyperparameter"]
-    self.current_version = version
+    def pre_execution_hook(self, version, model_dir, exec_mode=ExecutionModeKeys.TEST):
+        self.log("Pre execution")
+        self.log("Version spec: {}".format(version))
+        self.model.hyperparameter = version["hyperparameter"]
+        self.current_version = version
 
-  def get_current_version(self):
-    return self.current_version
+    def get_current_version(self):
+        return self.current_version
 
-  def get_trained_step_count(self):
-    return 10
+    def get_trained_step_count(self):
+        return 10
 
-  def train_model(self, input_fn, steps):
-    self.log("steps: ", steps)
-    self.log("calling input fn")
-    input_fn()
-    self.log("trained: {}".format(self.model.train()))
+    def train_model(self, input_fn, steps):
+        metric_container = MetricContainer(metrics =  ['1', 'b', 'c'], track_average_epoc_count = 5)
+        metric_container = MetricContainer(metrics = [{'metrics': ['a', 'b', 'c']}, {'metrics': ['2', 'd', 'e'], 'track_average_epoc_count': 10}], track_average_epoc_count = 5)
+        self.log("steps: {}".format(steps))
+        self.log("calling input fn")
+        input_fn()
+        for epoc in range(3):
+            for idx in range(6):
+                metric_container.a.update(idx)
+                metric_container.b.update(idx*2)
+                self.log("Epoc: {}   step: {}".format(epoc, idx))
+                self.log("a {}".format(metric_container.a.avg()))
+                self.log("b {}".format(metric_container.b.avg()))
+                
+                if idx%3 == 0:
+                    metric_container.reset()
+            metric_container.reset_epoc()
+                
+        self.log("trained: {}".format(self.model.train()))
 
-  def evaluate_model(self, input_fn, steps):
-    self.log("steps: ", steps)
-    self.log("calling input fn")
-    input_fn()
+    def evaluate_model(self, input_fn, steps):
+        self.log("steps: {}".format(steps))
+        self.log("calling input fn")
+        input_fn()
 
 dl = TestingDataLoader()
-v = Versions(0.01, dl, 1, 1)
+v = Versions(0.01, dl, 1, 10)
 v.addV("version1", custom_paramters = {"hyperparameter": "a hyperparameter"})
 v.addV("version2", custom_paramters = {"hyperparameter": None})
 MODEL = TestingModel(versions = v)
