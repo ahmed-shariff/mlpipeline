9180330c1258188229d324282de4399ac2a487a4
Ahmed Shariff <ahmed.shariff@live.com>
2019-04-24 13:16:49 +0530

    New release 1.1.a.3.post.9

--- mlpipeline/__init__.py
+++ mlpipeline/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.a.3.post.8"
+__version__ = "1.1.a.3.post.9"
 
 from mlpipeline._api_interface import (mlpipeline_execute_exeperiment,
                                        mlpipeline_execute_pipeline)
--- reinstall.sh
+++ reinstall.sh
@@ -1,4 +1,4 @@
 # Simple helper script to reinstall and test the package
 python setup.py sdist bdist_wheel
 pip uninstall mlpipeline -y
-pip install dist/mlpipeline-1.1a3.post8-py3-none-any.whl
+pip install dist/mlpipeline-1.1a3.post9-py3-none-any.whl
