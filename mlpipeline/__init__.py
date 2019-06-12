057a59bbc731b344d60e68cad02c213acc732eb2
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2019-06-12 21:38:02 +0530

    New release 1.1.a.3.post.12

--- mlpipeline/__init__.py
+++ mlpipeline/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.1.a.3.post.11"
+__version__ = "1.1.a.3.post.12"
 
 from mlpipeline._api_interface import (mlpipeline_execute_exeperiment,
                                        mlpipeline_execute_pipeline)
