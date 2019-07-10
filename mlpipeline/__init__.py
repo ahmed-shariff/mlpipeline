71e7d84312ab924306876c97941bd97150e2a1e6
Ahmed Shariff <ahmed.shariff@live.com>
2019-07-10 13:47:30 +0530

    New release 2.0.a.2

--- mlpipeline/__init__.py
+++ mlpipeline/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.a.1"
+__version__ = "2.0.a.2"
 
 from mlpipeline._api_interface import (mlpipeline_execute_exeperiment,
                                        mlpipeline_execute_pipeline)
